0dc515ae752aec3126c5c0479d99b1f32fe2099c
chickengak <37939855+chickengak@users.noreply.github.com>
2018-11-24 00:31:00 +0900

    AddWeight_with_comment
    just added long explaination and idea and how to.
    you had better read my excel file
    we have to write code.

--- HDT_Puzzle/HDT_Puzzle/HidatoMain.cpp
+++ HDT_Puzzle/HDT_Puzzle/HidatoMain.cpp
@@ -8,27 +8,33 @@ using namespace std;
 
 int main(void)
 {
-	srand((unsigned int)time(NULL));
-	Generator Gen;
-	int startX = Gen.getStartRow();
-	int startY = Gen.getStartCol();
-	Gen.GeneratorPuzzle(startX, startY, START_NUM);
-	Gen.getEndNumber();
-	Gen.ShowPuzzle();
-	Gen.ShowCompletePuzzle();
-	Gen.ShowHint();
+	int tc = 100;
+	clock_t total = 0;
+	while (tc--) {
+		srand((unsigned int)time(NULL));
+		Generator Gen;
+		int startX = Gen.getStartRow();
+		int startY = Gen.getStartCol();
+		Gen.GeneratorPuzzle(startX, startY, START_NUM);
+		//Gen.getEndNumber();
+		//Gen.ShowPuzzle();
+		//Gen.ShowCompletePuzzle();
+		//Gen.ShowHint();
 
-	Solver sol;
-	//sol.setPuzzlePtr(Gen.getPuzzle());
-	sol.setSolPuzzle(Gen.getPuzzle(), 15, 15); // row, col size 변수 선언해야함
-	cout << sol.getMax() << endl;
-	cout << sol.getEnd().first << " " << sol.getEnd().second << endl;
-	sol.ShowPuzzle();
-	clock_t begin, end;
-	begin = clock();
-	sol.Initiate();
-	end = clock();
-	sol.ShowPuzzle();
-	cout << endl << "Time: " << end - begin << "ms" << endl;
+		Solver sol;
+		//sol.setPuzzlePtr(Gen.getPuzzle());
+		sol.setSolPuzzle(Gen.getPuzzle(), 15, 15); // row, col size 변수 선언해야함
+		cout << sol.getMax() << endl;
+		//cout << sol.getEnd().first << " " << sol.getEnd().second << endl;
+		////sol.ShowPuzzle();
+		clock_t begin, end;
+		begin = clock();
+		sol.Initiate();
+		end = clock();
+		////sol.ShowPuzzle();
+		cout << endl << "Time: " << end - begin << "ms" << endl;
+		total += (end - begin);
+	}
+	cout << "average:: " << total / tc << endl;
 	return 0;
 }
